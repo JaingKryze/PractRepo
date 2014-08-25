4ed55e8a55d030cf4fbc11570851fae5e36248da
JaingKryze <speedskatercanbefound@gmail.com>
2014-08-25 07:09:59 -0700

    3rd commit

--- GitHub Practice Project/src/GitHubPractice.java
+++ GitHub Practice Project/src/GitHubPractice.java
@@ -3,7 +3,7 @@ public class GitHubPractice
 	{
 	public static void main(String[] args)
 		{
-		System.out.println("I modified this at school.");
+		System.out.println("I modified this at school and at home.");
 		
 		//Add a line about modifying it at school, then upload it
 		// back to GitHub.
@@ -11,7 +11,7 @@ public class GitHubPractice
 		System.out.println("hi");
 		//Open this at home and add a line about modifying it there.
 		//Update it on GitHub.
-		
+		System.out.println("SAO is best anime");
 		//Open it up back at school to demonstrate you can move
 		//code back and forth and modify it both places.
 
